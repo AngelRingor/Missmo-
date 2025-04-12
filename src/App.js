7909019864efdd7c46e59c86f59319fda45beade
AngelRingor <asringor@paterostechnologicalcollege.edu.ph>
2025-04-12 17:24:22 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [answer, setAnswer] = useState(null);
+
+  const handleClick = (response) => {
+    setAnswer(response);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      {!answer && (
+        <>
+          <h1>Miss mo?</h1>
+          <div className="btn-group">
+            <button onClick={() => handleClick('yes')}>Yes</button>
+            <button onClick={() => handleClick('no')}>No</button>
+          </div>
+        </>
+      )}
+
+      {answer === 'yes' && <p className="response">Miss ka ba? 😭</p>}
+      {answer === 'no' && <p className="response">Sus, panggap si tanga 😔</p>}
     </div>
   );
 }
